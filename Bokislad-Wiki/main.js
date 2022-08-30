b8c61882ca8a0d0c62da78f6f85e6d9689608464
wedsney <wedsney22@gmail.com>
2022-08-30 20:38:58 -0300

    limpando

--- Bokislad-Wiki/main.js
+++
@@ -1,9 +0,0 @@
-function show() {
-
-    let image = document.getElementById("image");
-
-    image.src ="Mapa_Bokislad.jpg"
-
-    document.getElementById("btnID")
-            .style.display = "none";
-}
